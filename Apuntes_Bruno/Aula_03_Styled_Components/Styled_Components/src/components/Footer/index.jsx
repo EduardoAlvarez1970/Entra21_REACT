745525f96c1640c75d19e4784d007cc03df4774c
Eduardo Alvarez <opticoedu@gmail.com>
2023-11-25 15:18:51 -0300

    Footer estilizado con styled-components, elimino el CSS

--- Apuntes_Bruno/Aula_03_Styled_Components/Styled_Components/src/components/Footer/index.jsx
+++ Apuntes_Bruno/Aula_03_Styled_Components/Styled_Components/src/components/Footer/index.jsx
@@ -1,23 +1,39 @@
 
-import "./styles.css"
+import styled from "styled-components";
 
 import line from '../../images/line1.svg'
 
 import { Menu } from '../Menu'
+
+const Foot = styled.footer`
+    width: 592px;
+    height: 36px;
+    top: 651px;
+    left: 340px;
+    border: 1px;
+`;
+
+const Linea = styled.img`
+    width: 568px;
+    top: 651px;
+    left: 364px;
+    border: none;
+    color: #ECEFF2;
+`;
 
 
 export function Footer() {
 
     return (
 
-       <div id="footer">
+       <Foot>
 
-          <img id="line" src={line} alt="linha divisora" />
+          <Linea src={line} alt="linha divisora" />
 
           <Menu/>
 
 
-       </div>
+       </Foot>
        
     )
        
