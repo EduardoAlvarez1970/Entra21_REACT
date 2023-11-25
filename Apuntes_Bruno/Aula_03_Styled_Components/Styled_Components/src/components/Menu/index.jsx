58e2d01a1ac6100163a9a386adccf484e97eac4f
Eduardo Alvarez <opticoedu@gmail.com>
2023-11-25 15:40:12 -0300

    Menu estilizado con styled components, elimino el css

--- Apuntes_Bruno/Aula_03_Styled_Components/Styled_Components/src/components/Menu/index.jsx
+++ Apuntes_Bruno/Aula_03_Styled_Components/Styled_Components/src/components/Menu/index.jsx
@@ -1,14 +1,38 @@
 
-import "./styles.css"
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    width: 592px;
+    height: 28px;
+    top: 659px;
+    left: 340px;
+    text-decoration: none;
+`;
+
+const Link = styled.a`
+    font-family: Open Sans;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 28px;
+    letter-spacing: 0em;
+    text-align: center;
+    padding: 6px 28px;
+    color: #FF9900;
+`;
+
+
+
+
+
 
 export function Menu() {
 
     return (
     
-       <div id="links">
-            <a target="_blank" href="">Instagram</a>
-            <a target="_blank" href="">Fale conosco</a>
-       </div>
+       <Wrapper>
+            <Link target="_blank" href="">Instagram</Link>
+            <Link target="_blank" href="">Fale conosco</Link>
+       </Wrapper>
 
     )
 }
