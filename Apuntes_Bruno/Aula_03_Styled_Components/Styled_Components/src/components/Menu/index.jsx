048caa3c8b96a060664392b8fed6af0a8840f747
Eduardo Alvarez <opticoedu@gmail.com>
2023-11-28 13:58:27 -0300

    Revert "final"
    This reverts commit a9ee4bff98164988260394fcabaafd1ed9e2ea61.

---
+++ Apuntes_Bruno/Aula_03_Styled_Components/Styled_Components/src/components/Menu/index.jsx
@@ -0,0 +1,17 @@
+
+import { Link, Wrapper } from './styles.js'
+
+export function Menu() {
+
+    return (
+       
+        <>
+        <Wrapper>
+            <Link target="_blank" href="">Instagram</Link>
+            <Link target="_blank" href="">Fale conosco</Link>
+       </Wrapper>
+        
+        </> 
+
+    )
+}
